3f7545bdcb5d7c10b0eacd9bbf5faf0f17402363
Brandon Fries <brandonfries1993@gmail.com>
2019-06-07 17:40:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,7 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    array.push("I am a strange loop.")
-  }
+  for (let i = 0; i < t; i++) {
+   array.push("I am a strange loop.")
+ }
+
+ return [array, t]
 }
